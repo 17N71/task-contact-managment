baf99ae828217f054b5036cdda3e6cb018dc8390
17N71 <narek07012020@gmail.com>
2025-01-15 18:50:05 +0400

    prettier fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,14 @@
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-   TanStackRouterVite(),
-   react()],  
-    resolve: {
-      alias:{
-        "~": path.resolve(__dirname, "./src")
-      }
-    }
-  }
-)
+  plugins: [TanStackRouterVite(), react()],
+  resolve: {
+    alias: {
+      "~": path.resolve(__dirname, "./src"),
+    },
+  },
+});
